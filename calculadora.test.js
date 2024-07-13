9e981b476f29368711c6a1da8196e02b14aebc4c
Gabriela Tavares <gabriela.ttavares@gmail.com>
2024-07-13 12:00:09 -0300

    test: add calculadora test

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,21 +1,25 @@
-const Calculadora = require('./calculadora')
+const Calculadora = require("./calculadora");
 
-describe('Calculadora', () => {
-  it('Deverá retornar a soma de dois números', () => {
-    //Seu código aqui
-    
-  })
+describe("Calculadora", () => {
+  let calculadora;
 
-  it('Deverá retornar a subtração de dois números', () => {
-    //Seu código aqui
-    
-  })
+  beforeEach(() => {
+    calculadora = new Calculadora();
+  });
 
-  it('Deverá retornar a multiplicação entre dois números', () => {
-    //Seu código aqui
-  })
+  it("Deverá retornar a soma de dois números", () => {
+    expect(calculadora.soma(2, 2)).toBe(4);
+  });
 
-  it('Deverá retornar a divisao entre dois números', () => {
-    //Seu código aqui
-  })
-})
+  it("Deverá retornar a subtração de dois números", () => {
+    expect(calculadora.subtracao(10, 2)).toBe(8);
+  });
+
+  it("Deverá retornar a multiplicação entre dois números", () => {
+    expect(calculadora.multiplicacao(5, 5)).toBe(25);
+  });
+
+  it("Deverá retornar a divisao entre dois números", () => {
+    expect(calculadora.divisao(100, 10)).toBe(10);
+  });
+});
